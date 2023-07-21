71f0ed944fb6c8cf6b17e60017f305009adcb552
flsnrtmmyungju <mjmj01617@naver.com>
2023-07-21 11:29:47 +0900

    gatsby-source-filesystem플러그인 설치해서 사용

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-init
+플러그인 추가로 설치
+로컬 파일 시스템에서 Gatsby 애플리케이션으로 데이터를 sourcing하기 위한 Gatsby 소스 플러그인.
+플러그인은 파일에서 파일 노드를 생성합니다.
+다양한 "transformer" 플러그인은 파일 노드를 다양한 다른 유형의 데이터로 변환할 수 있습니다.
+npm install gatsby-source-filesystem
+
+gatsby-config.ts파일에서
+plugins: [
+{
+resolve: `gatsby-source-filesystem`,
+options: {
+path: `${__dirname}/blog-posts`,//폴더위치원하는곳 수정(src바깥인지 경로확인필수)
+},
+},
+],
+
+https://www.gatsbyjs.com/plugins/gatsby-source-filesystem
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,30 @@
-import { HeadFC } from "gatsby";
+import { graphql, HeadFC, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+  console.log("data", data);
   return (
     <Layout title="Blog">
-      <p>Blog</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+//추가로설치한 플로그인 gatsby-source-filesystem 때문에 사용가능
+//위의 data에 들어감
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head: HeadFC = () => <title>Blog | DevStickers!</title>;
